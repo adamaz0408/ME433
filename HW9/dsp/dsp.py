d122de57860410c1e04393ec21ad18e75eae762e
Adam Azhar <adamazhar2027@u.northwestern.edu>
2026-06-07 18:07:23 -0500

    iImplemented FFT and plotted frequency domains for step 4, added .png images for plots

--- HW9/dsp/dsp.py
+++ HW9/dsp/dsp.py
@@ -1,11 +1,12 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 # load data for plotting
 t = []
 signal = []
 
-filename = 'dsp/csv_data/sigA.csv'
+filename = 'dsp/csv_data/sigD.csv'
 
 with open(filename, 'r') as f:
     reader = csv.reader(f)
@@ -26,10 +27,34 @@ print(f"Total Data Points: {num_points}")
 print(f"Total Time: {total_time} seconds")
 print(f"Calculated Sample Rate (Fs): {sample_rate} Hz")
 
-# plot raw data to prove it worked
-plt.plot(t, signal, 'k')
-plt.xlabel('Time [s]')
-plt.ylabel('Amplitude')
-plt.title(f'Raw Signal vs Time ({filename})')
-plt.grid(True)
+# FFT calc
+# create freq x-axis 
+k = np.arange(num_points)
+T = num_points / sample_rate
+frq = k / T
+frq = frq[range(int(num_points / 2))] 
+
+# calc FFT mags (y-axis)
+Y = np.fft.fft(signal) / num_points
+Y = Y[range(int(num_points / 2))]
+
+# plotting both subplots
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+
+# signal v time plot
+ax1.plot(t, signal, 'k')
+ax1.set_xlabel('Time [s]')
+ax1.set_ylabel('Amplitude')
+ax1.set_title(f'Raw Signal vs Time ({filename})')
+ax1.grid(True)
+
+# FFT plot
+ax2.plot(frq, abs(Y), 'b') 
+ax2.set_xlabel('Freq (Hz)')
+ax2.set_ylabel('|Y(freq)|')
+ax2.set_title('FFT (Frequency Domain)')
+ax2.set_xlim(0, 500) # limit for visibility
+ax2.grid(True)
+
+plt.tight_layout()
 plt.show()
